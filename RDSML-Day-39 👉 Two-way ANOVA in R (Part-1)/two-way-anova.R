0f7a1df61ab153591a8ef7a816f554481c2e574e
Sadhan Verma <sadhan.hstu@gmail.com>
2025-09-15 22:28:52 +0600

    Update

--- RDSML-Day-39 👉 Two-way ANOVA in R (Part-1)/two-way-anova.R
+++ RDSML-Day-39 👉 Two-way ANOVA in R (Part-1)/two-way-anova.R
@@ -1,2 +1,11 @@
 # installing Packages
-install.packages(c("tidyverse", "car", "emmeans", "effectsize", "performance", "broom", "kableExtra"))
+# install.packages(c("tidyverse", "car", "emmeans", "effectsize", "performance", "broom", "kableExtra"))
+library(tidyverse)
+
+# Importing the dataset
+d <- read.csv("cropdata.csv")
+
+# Quick Exploratory Descriptive Analysis
+summs <- d %>%
+  group_by(fert, irr) %>%
+  summarise(mean = mean(y), se = sd(y)/sqrt(n()), .groups = "drop")
