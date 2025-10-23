a61bace66648b05cf3db4ca038770f88d18dc2b1
Sadhan Verma <sadhan.hstu@gmail.com>
2025-10-23 16:47:15 +0600

    Add files via upload

--- RDSML-Day-48 👉 Multiple Linear Regression in R (Part-1)/multiple_linear_regression.R
+++ RDSML-Day-48 👉 Multiple Linear Regression in R (Part-1)/multiple_linear_regression.R
@@ -1,22 +1,26 @@
 # Multiple Linear Regression
 
 # Importing the dataset
-dataset <- read.csv("50_Startups.csv")
+dataset = read.csv('50_Startups.csv')
 
-# Encoding the categorical data
-dataset$State <- factor(dataset$State,
-                        levels = c('New York', 'California', 'Florida'),
-                        labels = c(1, 2, 3))
+# Encoding categorical data
+dataset$State = factor(dataset$State,
+                       levels = c('New York', 'California', 'Florida'),
+                       labels = c(1, 2, 3))
 
-# Splitting the dataset into the Training Set and Test Set
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
 library(caTools)
 set.seed(123)
-split <- sample.split(dataset$Profit, SplitRatio = 0.8)
-training_set <- subset(dataset, split == TRUE)
-test_set <- subset(dataset, split == FALSE)
+split = sample.split(dataset$Profit, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
 
-# Fitting the Multiple Linear Regression Model to the Training Set
-regressor <- lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State, 
-                data = training_set)
+# Fitting Multiple Linear Regression to the Training set
+regressor = lm(formula = Profit ~ .,
+               data = training_set)
+
 summary(regressor)
 
+# Predicting the Test set results
+y_pred = predict(regressor, newdata = test_set)
